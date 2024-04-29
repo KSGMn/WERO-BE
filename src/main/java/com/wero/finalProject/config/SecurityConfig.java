ba881e40c23745d0745697109f55b3bdd9f6e8e9
HyeonAm <oha949500@gmail.com>
2024-04-29 09:40:43 +0900

    exception 핸들링 로직 수정

--- src/main/java/com/wero/finalProject/config/SecurityConfig.java
+++ src/main/java/com/wero/finalProject/config/SecurityConfig.java
@@ -6,10 +6,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -19,7 +19,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -46,7 +45,7 @@ public class SecurityConfig {
     private OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
-    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+    protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{ //FilterChain
         httpSecurity
                 .cors(cors -> cors
                         .configurationSource(corsConfigurationSource()))
@@ -54,12 +53,13 @@ public class SecurityConfig {
                 .httpBasic(HttpBasicConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authorizeHttpRequests(request -> request.requestMatchers("/", "/api/v1/auth/**", "/ouath2/**")
+                .authorizeHttpRequests(request -> request.requestMatchers("/", "/api/v1/auth/**", "/ouath2/**", "/file/**")
                         .permitAll()
                         .requestMatchers("/api/v1/user/**").hasRole("USER")
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/board/**", "api/v1/user/*").permitAll()
                         .anyRequest()
-                        .authenticated());
+                        .authenticated()).exceptionHandling(exceptionHandling->exceptionHandling.authenticationEntryPoint(new FailedAuthenticationEntryPoint()));
 
 //TODO: Oauth2 소셜 로그인 yml 설정 추가후 주석 해제
 
