461a53dfedc56af7646d7af4d7dba8cebc777be7
HyeonAm <oha949500@gmail.com>
2024-05-03 11:15:23 +0900

    필수 주석 수정

--- src/main/java/com/wero/finalProject/service/UserService.java
+++ src/main/java/com/wero/finalProject/service/UserService.java
@@ -8,8 +8,8 @@ import org.springframework.http.ResponseEntity;
  * @작성자:오현암
  * @작성날짜:2024/05/03
  * @파일명:UserService.Interface
+ * @기능:유저_수정_서비스_로직_생성
  **/
 public interface UserService {
-
     ResponseEntity<? super UserUpdateResponseDto> userUpdate(UserUpdateRequestDto dto, String userId);
 }
