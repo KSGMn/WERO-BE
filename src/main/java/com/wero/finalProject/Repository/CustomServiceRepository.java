294436d878a8c034d99f5e1a423f2767cd44432a
HyeonAm <oha949500@gmail.com>
2024-05-03 11:09:55 +0900

    필수 주석 생성

--- src/main/java/com/wero/finalProject/Repository/CustomServiceRepository.java
+++ src/main/java/com/wero/finalProject/Repository/CustomServiceRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * @작성자:
- * @작성날짜:
- * @파일명:
+ * @작성자:오현암
+ * @작성날짜:2024/05/02
+ * @파일명:CustomServiceRepository.interface
+ * @기능:CustomServiceRepository<->JPA
  **/
 
 @Repository
