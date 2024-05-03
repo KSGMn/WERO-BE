b396a121d7eb7dfd4c4926c55f4cd31d94976a7e
HyeonAm <oha949500@gmail.com>
2024-05-03 11:16:16 +0900

    필수 주석 수정, 불필요한 import문, 변수 제거

--- src/main/java/com/wero/finalProject/service/implement/UserServiceImpl.java
+++ src/main/java/com/wero/finalProject/service/implement/UserServiceImpl.java
@@ -5,7 +5,6 @@ import com.wero.finalProject.domain.UserEntity;
 import com.wero.finalProject.dto.request.user.UserUpdateRequestDto;
 import com.wero.finalProject.dto.response.ResponseDto;
 import com.wero.finalProject.dto.response.user.UserUpdateResponseDto;
-import com.wero.finalProject.provider.JwtProvider;
 import com.wero.finalProject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +16,7 @@ import org.springframework.stereotype.Service;
  * @작성자:오현암
  * @작성날짜:2024/05/03
  * @파일명:UserServiceImpl.class
+ * @기능:유저_수정_서비스_로직_상세구현
  **/
 
 @Service
@@ -28,9 +28,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseEntity<? super UserUpdateResponseDto> userUpdate(UserUpdateRequestDto dto, String userId) {
-        String token = null;
         try {
-
             UserEntity user = userRepository.findByUserId(userId);
             if (user == null) return UserUpdateResponseDto.notExistUser();
 
