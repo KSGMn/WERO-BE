b2e46c779cdf2587041e2fe521e68c1e3afc3b67
Hyeon-Am.OH <117336788+BasaltAm@users.noreply.github.com>
2024-06-03 15:55:34 +0900

    Update SecurityConfig.java

--- src/main/java/com/wero/finalProject/config/SecurityConfig.java
+++ src/main/java/com/wero/finalProject/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig{
                                                 .requestMatchers("/", "/api/v1/auth/**", "/api/v1/cs/**", "/uploads/**",
                                                                 "/oauth2/**", "/api/v1/nonuser/**")
                                                 .permitAll()
-                                                .requestMatchers("/api/v1/user/**").hasAnyRole("USER", "ADMIN")
+                                                .requestMatchers("/api/v1/user/**").hasRole("USER")
                                                 .requestMatchers("/api/v1/user/**").hasRole("ADMIN")
                                                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                                                 .anyRequest().authenticated()
